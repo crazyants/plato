10936eecd6a9761182b58822256a728d8de94458
InstantASP <saes@instantasp.co.uk>
2019-01-07 22:06:01 +0000

    Adding missing js files to repo

---
+++ src/Plato/wwwroot/js/app/locale/app.en-us.js
@@ -0,0 +1,9 @@
+﻿$(function(win) {
+
+    win.$.Plato.Strings = {
+        'An error occurred!': "An error occurred!",
+        'Information is provided below...': "Information is provided below...",
+        'Could not authenticate your request!': "Could not authenticate your request!"
+    };
+
+}(window))
