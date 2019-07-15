99f64798bd5e2308118d07def846679d22b9e601
InstantASP <sales@instantasp.co.uk>
2019-07-15 19:08:07 +0100

    Further work on search help

---
+++ src/Plato.Internal.Repositories/IQueryableRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using Plato.Internal.Data.Abstractions;
+
+namespace Plato.Internal.Repositories
+{
+
+    /// <summary>
+    /// Represents a repository that supports an IPagedResults.
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public interface IQueryableRepository<TModel> where TModel : class
+    {
+        Task<IPagedResults<TModel>> SelectAsync(IDbDataParameter[] dbParams);
+
+    }
+
+}
--- src/Plato.Internal.Repositories/IRepository.cs
+++ src/Plato.Internal.Repositories/IRepository.cs
@@ -4,17 +4,7 @@ using Plato.Internal.Data.Abstractions;
 
 namespace Plato.Internal.Repositories
 {
-
-    /// <summary>
-    /// Represents a repository that supports an IPagedResults.
-    /// </summary>
-    /// <typeparam name="TModel"></typeparam>
-    public interface IQueryableRepository<TModel> where TModel : class
-    {
-        Task<IPagedResults<TModel>> SelectAsync(IDbDataParameter[] dbParams);
-
-    }
-
+    
     /// <summary>
     /// Represents a repository that supports creating, deleting & querying data.
     /// </summary>
