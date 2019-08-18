72a93b7f7e05f4b827e4a57d41e8f9a1056d9bfa
InstantASP <sales@instantasp.co.uk>
2019-08-18 16:04:25 +0100

    Fixes to categories + roles in various modules

---
+++ src/Plato/Modules/Plato.Articles.Categories/ViewComponents/ArticleCategoryDropDownViewComponent.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Plato.Categories.Models;
+using Plato.Categories.Services;
+using Plato.Categories.ViewModels;
+using Plato.Articles.Categories.Models;
+using Plato.Internal.Navigation.Abstractions;
+
+namespace Plato.Articles.Categories.ViewComponents
+{
+
+    public class ArticleCategoryDropDownViewComponent : ViewComponent
+    {
+     
+        private readonly ICategoryService<Category> _categoryService;
+
+        public ArticleCategoryDropDownViewComponent(ICategoryService<Category> categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(CategoryDropDownViewModel model)
+        {
+
+            if (model == null)
+            {
+                model = new CategoryDropDownViewModel();
+            }
+
+            if (model.SelectedCategories == null)
+            {
+                model.SelectedCategories = new int[0];
+            }
+            
+            model.Categories = await BuildSelectionsAsync(model);
+
+            return View(model);
+
+        }
+
+        private async Task<IList<Selection<CategoryBase>>> BuildSelectionsAsync(CategoryDropDownViewModel model)
+        {
+           
+            // Get categories
+            var categories = await _categoryService.GetResultsAsync(
+                model.Options, new PagerOptions()
+                {
+                    Page = 1,
+                    Size = int.MaxValue
+                });
+
+            // Indicate selections
+            return categories?.Data?.Select(c => new Selection<CategoryBase>
+                {
+                    IsSelected = model.SelectedCategories.Any(v => v == c.Id),
+                    Value = c
+                })
+                .ToList();
+
+        }
+
+    }
+    
+}
+
---
+++ src/Plato/Modules/Plato.Issues.Categories/ViewComponents/IssueCategoryTreeViewComponent.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Plato.Categories.Models;
+using Plato.Categories.Services;
+using Plato.Categories.ViewModels;
+using Plato.Issues.Categories.Models;
+using Plato.Internal.Navigation.Abstractions;
+
+namespace Plato.Issues.Categories.ViewComponents
+{
+
+    public class IssueCategoryTreeViewComponent : ViewComponent
+    {
+
+        private readonly ICategoryService<Category> _categoryService;
+     
+        public IssueCategoryTreeViewComponent(ICategoryService<Category> categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(CategoryTreeOptions options)
+        {
+
+            if (options == null)
+            {
+                options = new CategoryTreeOptions();
+            }
+
+            if (options.SelectedCategories == null)
+            {
+                options.SelectedCategories = new int[0];
+            }
+
+            return View(new CategoryTreeViewModel
+            {
+                HtmlName = options.HtmlName,
+                EnableCheckBoxes = options.EnableCheckBoxes,
+                EditMenuViewName = options.EditMenuViewName,
+                SelectedCategories = await BuildCategories(options),
+                CssClass = options.CssClass,
+                RouteValues = options.RouteValues
+            });
+
+        }
+        
+
+        private async Task<IList<Selection<CategoryBase>>> BuildCategories(CategoryTreeOptions options)
+        {
+
+            // Get categories
+            var categories = await _categoryService
+                .GetResultsAsync(options.IndexOptions, new PagerOptions()
+                {
+                    Page = 1,
+                    Size = int.MaxValue
+                });
+
+            return categories?.Data.Select(c => new Selection<CategoryBase>
+                {
+                    IsSelected = options.SelectedCategories.Any(v => v == c.Id),
+                    Value = c
+                })
+                .ToList();
+
+        }
+
+    }
+    
+}
